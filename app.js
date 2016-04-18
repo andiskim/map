c70c79e11f4b66da9227be7d0336f0d5883913d5
Andy <andiskim@gmail.com>
2016-04-18 21:25:08 +0900

    commit before making changes to Wikipedia API on infoWindow

--- app.js
+++ app.js
@@ -1,13 +1,68 @@
+// GOOGLE MAPS!
+
 function initMap() {
-  // Create a map object and specify the DOM element for display.
+  var myLatLng = {lat: model.locations[0].lat, lng: model.locations[0].long};
+
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    scrollwheel: false,
-    zoom: 8
+    zoom: 1,
+    center: myLatLng
   });
+
+  // create a new infowindow variable
+  var infowindow = new google.maps.InfoWindow();
+
+  // create marker and i global variables
+  var marker, i;
+
+  for (i = 0; i < model.locations.length; i++) {
+     marker = new google.maps.Marker({
+       position: new google.maps.LatLng(model.locations[i].lat, model.locations[i].long),
+       map: map
+     });
+
+     google.maps.event.addListener(marker, 'click', (function(marker, i) {
+       return function() {
+         // ADD CONTENT OF THE INFORMATION WINDOW
+         var http = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ model.locations[i].city + ", " + model.locations[i].country + "&format=json";
+         // PARSE JSON
+         $.ajax({
+          type: "GET",
+          url: http,
+          contentType: "application/json; charset=utf-8",
+          async: false,
+          dataType: "jsonp",
+          success: function (data, textStatus, jqXHR) {
+              console.log(data);
+          },
+          error: function (errorMessage) {
+        }
+    });
+
+         infowindow.setContent("<h4>Wikipedia Results</h4>" +
+         http
+       );
+         infowindow.open(map, marker);
+         //ANIMATE MAP MARKERS WHEN CLICKED
+         // use closure to have a separate variable for animation
+         (function(markerClosure) {
+           if (markerClosure.getAnimation() == null) {
+             markerClosure.setAnimation(google.maps.Animation.BOUNCE);
+           } else {
+             markerClosure.setAnimation(null);
+           }
+         })(marker);
+       }
+     })(marker, i));
+   }
+
+   function toggleBounce() {
+
+   }
 }
 
-// add model values
+
+
+// VIEW!
 // tag to id of list
 var list = document.getElementById("list");
 for (var i=0;i<model.locations.length;i++) {
@@ -15,6 +70,4 @@ for (var i=0;i<model.locations.length;i++) {
   var newli = document.createElement("li");
   // append a new li to the ul
   list.appendChild(newli).innerHTML = model.locations[i].city + ", " + model.locations[i].country;
-
-  console.log("hello");
 }
--- model.js
+++ model.js
@@ -2,23 +2,33 @@ var model = {
   locations: [
     {
       city: "Madrid",
-      country: "Spain"
+      country: "Spain",
+      lat: 40.4378693,
+      long: -3.8199645
     },
     {
       city: "Bogota",
-      country: "Colombia"
+      country: "Colombia",
+      lat: 3.1699298,
+      long: -76.0765486
     },
     {
       city: "Almaty",
-      country: "Kazakhstan"
+      country: "Kazakhstan",
+      lat: 43.1684119,
+      long: 74.6190092
     },
     {
       city: "Budapest",
-      country: "Hungary"
+      country: "Hungary",
+      lat: 47.4580173,
+      long: 18.265469
     },
     {
       city: "Warsaw",
-      country: "Poland"
+      country: "Poland",
+      lat: 52.2215236,
+      long: 20.0824175
     }
   ]
 };
