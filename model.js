bf1540ee09cd471f817e251b85b897350179ef9c
Andy <andiskim@gmail.com>
2016-04-20 18:49:31 +0900

    synchronized view and map filters

--- model.js
+++ model.js
@@ -1,3 +1,4 @@
+'use strict'
 // data
 var model = {
   locations: [
@@ -34,18 +35,97 @@ var model = {
   ]
 };
 
-// object constructor for knockoutjs
-function Location(data,i) {
-  this.city = ko.observable(data.city);
-  this.country = ko.observable(data.country);
-  this.lat = ko.observable(data.lat);
-  this.long = ko.observable(data.long);
+// INITIALIZE GOOGLE MAP
+var gmarkers = [];
+
+var myLatLng = {lat: model.locations[0].lat, lng: model.locations[0].long};
+
+var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 2,
+  center: myLatLng
+});
+
+var infowindow = new google.maps.InfoWindow();
+
+// OBJECT CONSTRUCTOR FOR LOCATION
+function Location($data,i) {
+  var marker;
+
+  this.city = ko.observable($data.city);
+  this.country = ko.observable($data.country);
+  this.lat = ko.observable($data.lat);
+  this.long = ko.observable($data.long);
   this.number = i;
   this.cityCountry = ko.computed(function() {
-    return this.city() + ", " + this.country()
+    return this.city() + ", " + this.country();
   }, this);
-};
 
+  // ADD NEW PIN
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng($data.lat, $data.long),
+    map: map,
+    draggable: false,
+    animation: google.maps.Animation.DROP
+  });
+
+  // GOOGLE MAPS LISTENER
+  // google.maps.event.addListener(marker, 'drag', function() {
+  //   var pos = marker.getPosition();
+  //   this.lat(pos.lat());
+  //   this.long(pos.lng());
+  // }.bind(this));
+  google.maps.event.addListener(marker, 'click', (function(marker) {
+    return function() {
+      // ADD CONTENT OF THE INFORMATION WINDOW
+      var http = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ $data.city + ", " + $data.country + "&format=json";
+      var wikiContent = "";
+      // PARSE JSON AND ADD DATA
+        $.ajax({
+           type: "GET",
+           url: http,
+           contentType: "application/json; charset=utf-8",
+           async: true,
+           dataType: "jsonp",
+           success: function (data, textStatus, jqXHR) {
+             for(var j=0; j < data[1].length; j++) {
+               wikiContent += "<li>" + (j+1).toString()+": "+ "<a href='" + data[3][j]+ "'>" + data[1][j] + "</a></li>";
+             }
+             // open window after loading data
+             infowindow.setContent("<h4>Wikipedia Results for "+ $data.city+", "+ $data.country +"</h4>" + wikiContent);
+             infowindow.open(map, marker);
+           },
+           fail: function (errorMessage) {
+             console.log("failed");
+           }
+       });
+
+       //ANIMATE MAP MARKERS WHEN CLICKED
+       // use closure to have a separate variable for animation
+       (function(markerClosure) {
+          // bounce animation
+          markerClosure.setAnimation(google.maps.Animation.BOUNCE);
+          // stop bounce after 1400ms
+          setTimeout(function(){ markerClosure.setAnimation(null); }, 1400);
+       })(marker);
+     }
+   })(marker));
+
+   //add to gmarkers
+   gmarkers.push(marker);
+
+  // HIDING PINS
+  this.isVisible = ko.observable(false);
+
+  this.isVisible.subscribe(function(currentState) {
+    if (currentState) {
+      marker.setMap(map);
+    } else {
+      marker.setMap(null);
+    }
+  });
+
+  this.isVisible(true);
+}
 
 //knockoutjs ViewModel to utilize model data and object constructor function
 function ViewModel() {
@@ -68,10 +148,14 @@ function ViewModel() {
   // use the query in the view to filter
   this.searchResults = ko.computed(function() {
     var q = self.Query();
-    return ko.utils.arrayFilter(self.locationsList(), function(i) {
-      return i.cityCountry().toLowerCase().indexOf(q) >= 0;
+    return ko.utils.arrayFilter(self.locationsList(), function(item) {
+      var match = item.cityCountry().toLowerCase().indexOf(q) >= 0;
+
+      item.isVisible(match);
+      return match;
+
     });
   });
-};
+}
 
 ko.applyBindings(new ViewModel());
