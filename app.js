e6ca4ddb15c028f8e46cd5926c6103976dde8abf
Andy <andiskim@gmail.com>
2016-04-21 16:54:05 +0900

    fixed menu bug

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ function errorHandling() {
   alert("Google MAPS API failed");
 }
 
-menu.addEventListener('click', function(e) {
-  drawer.classList.toggle('open');
-  e.stopPropagation();
-});
-
 var menu = document.querySelector('#menu');
 var main = document.querySelector('main');
 var drawer = document.querySelector('#drawer');
